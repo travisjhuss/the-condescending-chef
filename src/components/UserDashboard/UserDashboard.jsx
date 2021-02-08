64edd93237358db756d2a89f288d3a9b72f901f5
Travis Huss <travisjhuss@gmail.com>
2021-02-08 14:32:10 -0600

    drawer set up on UserDashboard

---
+++ src/components/UserDashboard/UserDashboard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+import LogOutButton from '../LogOutButton/LogOutButton';
+import { useEffect, useState } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Typography, Button, Drawer } from '@material-ui/core';
+
+function UserPage() {
+  // this component doesn't do much to start, just renders some user reducer info to the DOM
+  const user = useSelector((store) => store.user);
+
+  // state for drawer
+  const [ isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  }
+
+  return (
+    <Router>
+      <div className="container">
+        <Nav />
+        <Typography variant="h4" color="secondary">Welcome, {user.username}!</Typography>
+        
+        <LogOutButton className="btn" />
+        <Button onClick={() => toggleDrawer()}>DRAWER</Button>
+          <Drawer anchor="left" open={isDrawerOpen} onClose={() => toggleDrawer()}>
+            <p>Drawer is open!</p>
+          </Drawer>
+      </div>
+    </Router>
+  );
+}
+
+// this allows us to use <App /> in index.js
+export default UserPage;

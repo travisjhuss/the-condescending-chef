a3632218c3f7845d9267ddfdaf3a477ca4f28e47
Travis Huss <travisjhuss@gmail.com>
2021-02-09 11:03:26 -0600

    two different add components working with conditional rendering in AddRecipe

---
+++ src/components/AddOutsideRecipe/AddOutsideRecipe.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+// mui
+import {TextField, Button, IconButton, makeStyles, Typography} from '@material-ui/core';
+
+const useStyles = makeStyles({
+    input: {
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: '#94836d',
+            },
+            '&:hover fieldset': {
+                borderColor: ' #ad4830',
+                border:  '#a0432c 3px solid',
+            }
+        },
+        backgroundColor: '#fff4dd',
+        borderRadius: '3px',
+    }
+})
+
+function AddOutsideRecipe() {
+
+    const classes = useStyles();
+
+    return(
+        <div className="container add-recipe">
+            <Typography>Add outside</Typography>
+            <TextField
+                variant="filled"
+                label="Recipe Name"
+                className={classes.input}
+            >
+
+            </TextField>
+        </div>
+    )
+}
+
+export default AddOutsideRecipe;
--- src/components/AddRecipe/AddRecipe.jsx
+++ src/components/AddRecipe/AddRecipe.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
+import AddUserRecipe from '../AddUserRecipe/AddUserRecipe';
+import AddOutsideRecipe from '../AddOutsideRecipe/AddOutsideRecipe';
 // mui
-import {TextField, Button, IconButton, makeStyles} from '@material-ui/core';
+import { Dialog, Button, IconButton, makeStyles, Typography } from '@material-ui/core';
 
 import './AddRecipe.css';
 
@@ -12,26 +14,73 @@ const useStyles = makeStyles({
             },
             '&:hover fieldset': {
                 borderColor: ' #ad4830',
-                border:  '#a0432c 2px solid',
+                border: '#a0432c 3px solid',
             }
         },
         backgroundColor: '#fff4dd',
-        borderRadius: '6px',
+        borderRadius: '3px',
     }
 })
 
 function AddRecipe() {
+    // states to determine what recipe form to show
+    const [isUserRecipe, setIsUserRecipe] = useState(false);
+    const [isOutsideRecipe, setIsOutsideRecipe] = useState(false);
+
+    // toggle based on choose
+    // form responds based on that
+    // pop up dialog?
 
     const classes = useStyles();
 
-    return (
-        <div className="container add-recipe">
-            <TextField
-                variant="outlined"
-                className={classes.input}
-            >
+    const clickUserRecipeForm = () => {
+        console.log('clicked User recipe');
+        setIsUserRecipe(true);
+        setIsOutsideRecipe(false);
+    };
 
-            </TextField>
+    const clickOutsideRecipeForm = () => {
+        console.log('clicked outside recipe');
+        setIsUserRecipe(false);
+        setIsOutsideRecipe(true);
+    };
+
+    return (
+        <div className="add-recipe-container">
+            <center>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={clickUserRecipeForm}
+                >
+                    <Typography
+                        variant="h6"
+                        color="secondary"
+                    >Add your own recipe</Typography>
+                </Button>
+                {' '}
+                <Typography
+                    color="secondary"
+                    display="inline"
+                >
+                    Or
+                </Typography>
+                {' '}
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={clickOutsideRecipeForm}
+                >
+                    <Typography
+                        variant="h6"
+                        color="secondary"
+                    >
+                        Add an outside recipe</Typography>
+                </Button>
+                {isUserRecipe ? <AddUserRecipe/> : null}
+                {isOutsideRecipe? <AddOutsideRecipe/> : null}
+
+            </center>
         </div>
     )
 }

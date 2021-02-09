2f6096158c1a6c669f7ae7ec5ac5981dc41798d3
Travis Huss <travisjhuss@gmail.com>
2021-02-08 18:02:30 -0600

    set up useStyles for inputs in AddRecipe

--- src/components/AddRecipe/AddRecipe.jsx
+++ src/components/AddRecipe/AddRecipe.jsx
@@ -1,8 +1,38 @@
+import { useEffect, useState } from 'react';
+// mui
+import {TextField, Button, IconButton, makeStyles} from '@material-ui/core';
 
+import './AddRecipe.css';
+
+const useStyles = makeStyles({
+    input: {
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: '#94836d',
+            },
+            '&:hover fieldset': {
+                borderColor: ' #ad4830',
+                border:  '#a0432c 2px solid',
+            }
+        },
+        backgroundColor: '#fff4dd',
+        borderRadius: '6px',
+    }
+})
 
 function AddRecipe() {
+
+    const classes = useStyles();
+
     return (
-        <p>IN ADD RECIPE</p>
+        <div className="container add-recipe">
+            <TextField
+                variant="outlined"
+                className={classes.input}
+            >
+
+            </TextField>
+        </div>
     )
 }
 

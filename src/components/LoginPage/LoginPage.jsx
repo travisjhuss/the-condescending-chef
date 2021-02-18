b1afb0b698d7d9540d8a6837bf2612e3e6034114
Travis Huss <travisjhuss@gmail.com>
2021-02-18 09:48:07 -0600

    logo added to login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
-import { Typography, Button } from '@material-ui/core';
+// MUI
+import { Typography, Button, makeStyles } from '@material-ui/core';
+import LocalDiningIcon from '@material-ui/icons/LocalDining';
+
+const useStyles = makeStyles({
+  button: {
+    textTransform: 'none',
+  },
+  logo: {
+    color: '#94836d',
+    fontSize: '150px',
+    position: 'absolute',
+    marginLeft: '-70px',
+    marginTop: '-29px'
+  },
+})
 
 function LoginPage() {
   const history = useHistory();
+  const classes = useStyles();
 
   return (
-    <div style={{marginTop: '200px'}}>
+    <div >
+      <center style={{ marginTop: '100px', marginBottom: '50px' }}>
+        <div className="logo">
+          <LocalDiningIcon className={classes.logo} />
+        </div>
+        <div className="title">
+          <Typography color="secondary" variant="h6" style={{ fontWeight: '500' }}>The</Typography>
+          <Typography color="secondary" variant="h4" style={{ fontWeight: '500' }}>Condescending</Typography>
+          <Typography color="secondary" variant="h6" style={{ fontWeight: '500' }}>Chef</Typography>
+        </div>
+      </center>
       <LoginForm />
 
       <center>

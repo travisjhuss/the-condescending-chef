790f9a6514c08eba90f88cd512cfe9dc6e562b08
Travis Huss <travisjhuss@gmail.com>
2021-03-22 14:20:29 -0500

    readme written

--- README.md
+++ README.md
@@ -1,121 +1,55 @@
+# The Condescending Chef
 
-# EDA Project
-This version uses React, Redux, Express, Passport, and PostgreSQL (a full list of dependencies can be found in `package.json`).
+## Description
 
-We **STRONGLY** recommend following these instructions carefully. It's a lot, and will take some time to set up, but your life will be much easier this way in the long run.
+_Duration: 2 Week Sprint_
 
-## Use the Template for This Repository (Don't Clone)
+More and more people rely on the internet as a way to get recipes. Many of us just want a simple recipe and end up spending half an hour looking through food blog after food blog only to settle on a recipe that doesn't deliver at the end. A lot of time and energy goes into cooking and having it not work out can be deflating. 
 
-- Don't Fork or Clone. Instead, click the `Use this Template` button, and make a copy to your personal account.
+I had the amazing experience of working as a professional chef for 15 years and over time you hone the ability to read a recipe and understand it like some read Shakespeare. I can take one look at a recipe and see where the red flags are. 
+
+Thats where my idea for an app came from. An app built for professional chefs to give feedback to everyday home cooks on recipes they find online, or even their own personal recipes. 
+
+To see the fully functional site, please visit: [DEPLOYED VERSION OF APP](https://shielded-escarpment-88386.herokuapp.com/#/home)
 
 
-## Prerequisites
+### Prerequisites
 
-Before you get started, make sure you have the following software installed on your computer:
+Link to software that is required to install the app (e.g. node).
 
 - [Node.js](https://nodejs.org/en/)
-- [PostrgeSQL](https://www.postgresql.org/)
-- [Nodemon](https://nodemon.io/)
+- [PostgreSQL](https://www.postgresql.org/)
 
-## Create database and table
+## Installation
 
-Create a new database called `prime_app` and create a `user` table:
+1. Create a database named `the-condescending-chef`,
+2. The queries in the `database.sql` file are set up to create all the necessary tables and populate the needed data to allow the application to run correctly. The project is built on [Postgres](https://www.postgresql.org/download/), so you will need to make sure to have that installed. We recommend using Postico to run those queries as that was used to create the queries, 
+3. Open up your editor of choice and run an `npm install`
+4. Run `npm run server` in your terminal
+5. Run `npm run client` in your terminal
+6. The `npm run client` command will open up a new browser tab for you!
 
-```SQL
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
-);
-```
+## Usage
 
-If you would like to name your database something else, you will need to change `prime_app` to the name of your new database name in `server/modules/pool.js`
+1. Signup and create a user
+2. You can upload a recipe one of two ways. By going to _Add Recipe_ you can fill out a form of your own recipe, or provide a link to upload an outside source recipe. 
+3. On either form you have the option to click _Mark For Review_. Doing so will flag that recipe so a chef admin can see it and review it. 
+4. While you wait, there you can browse other recipes and read their reviews. 
+5. In the future, users will be able to favorite recipes and communicate directly with other users. 
+6. That's it. Easy!
 
-## Development Setup Instructions
 
-- Run `npm install`
-- Create a `.env` file at the root of the project and paste this line into the file:
-  ```
-  SERVER_SESSION_SECRET=superDuperSecret
-  ```
-  While you're in your new `.env` file, take the time to replace `superDuperSecret` with some long random string like `25POUbVtx6RKVNWszd9ERB9Bb6` to keep your application secure. Here's a site that can help you: [https://passwordsgenerator.net/](https://passwordsgenerator.net/). If you don't do this step, create a secret with less than eight characters, or leave it as `superDuperSecret`, you will get a warning.
-- Start postgres if not running already by using `brew services start postgresql`
-- Run `npm run server`
-- Run `npm run client`
-- Navigate to `localhost:3000`
+## Built With
 
-## Debugging
+- React
+- Node
+- Express
+- Material UI
+- PostgreSQL
+- Redux
 
-To debug, you will need to run the client-side separately from the server. Start the client by running the command `npm run client`. Start the debugging server by selecting the Debug button.
+## Acknowledgement
+Thanks to [Prime Digital Academy](www.primeacademy.io) who equipped and helped me to make this application a reality. Thank you to everyone in the Zhu cohort for sharing this experience and pushing each other to grow and be better. 
 
-![VSCode Toolbar](documentation/images/vscode-toolbar.png)
-
-Then make sure `Launch Program` is selected from the dropdown, then click the green play arrow.
-
-![VSCode Debug Bar](documentation/images/vscode-debug-bar.png)
-
-## Testing Routes with Postman
-
-To use Postman with this repo, you will need to set up requests in Postman to register a user and login a user at a minimum.
-
-Keep in mind that once you using the login route, Postman will manage your session cookie for you just like a browser, ensuring it is sent with each subsequent request. If you delete the `localhost` cookie in Postman, it will effectively log you out.
-
-1. Start the server - `npm run server`
-2. [Import the sample routes JSON file](./PostmanPrimeSoloRoutes.json) by clicking `Import` in Postman. Select the file.
-3. Click `Collections` and `Send` the following three calls in order:
-   1. `POST /api/user/register` registers a new user, see body to change username/password
-   2. `POST /api/user/login` will login a user, see body to change username/password
-   3. `GET /api/user` will get user information, by default it's not very much
-
-After running the login route above, you can try any other route you've created that requires a logged in user!
-
-## Production Build
-
-Before pushing to Heroku, run `npm run build` in terminal. This will create a build folder that contains the code Heroku will be pointed at. You can test this build by typing `npm start`. Keep in mind that `npm start` will let you preview the production build but will **not** auto update.
-
-- Start postgres if not running already by using `brew services start postgresql`
-- Run `npm start`
-- Navigate to `localhost:5000`
-
-## Lay of the Land
-
-There are a few videos linked below that show a walkthrough the client and sever setup to help acclimatize to the boilerplate. Please take some time to watch the videos in order to get a better understanding of what the boilerplate is like.
-
-- [Initial Set](https://vimeo.com/453297271)
-- [Server Walkthrough](https://vimeo.com/453297212)
-- [Client Walkthrough](https://vimeo.com/453297124)
-
-Directory Structure:
-
-- `src/` contains the React application
-- `public/` contains static assets for the client-side
-- `build/` after you build the project, contains the transpiled code from `src/` and `public/` that will be viewed on the production site
-- `server/` contains the Express App
-
-This code is also heavily commented. We recommend reading through the comments, getting a lay of the land, and becoming comfortable with how the code works before you start making too many changes. If you're wondering where to start, consider reading through component file comments in the following order:
-
-- src/components
-  - App/App
-  - Footer/Footer
-  - Nav/Nav
-  - AboutPage/AboutPage
-  - InfoPage/InfoPage
-  - UserPage/UserPage
-  - LoginPage/LoginPage
-  - RegisterPage/RegisterPage
-  - LogOutButton/LogOutButton
-  - ProtectedRoute/ProtectedRoute
-
-## Deployment
-
-1. Create a new Heroku project
-1. Link the Heroku project to the project GitHub Repo
-1. Create an Heroku Postgres database
-1. Connect to the Heroku Postgres database from Postico
-1. Create the necessary tables
-1. Add an environment variable for `SERVER_SESSION_SECRET` with a nice random string for security
-1. In the deploy section, select manual deploy
-
-## Update Documentation
-
-Customize this ReadMe and the code comments in this project to read less like a starter repo and more like a project. Here is an example: https://gist.github.com/PurpleBooth/109311bb0361f32d87a2
+## Support
+If you have suggestions or issues, please email me at [travisjhuss@gmail.com](travisjhuss@gmail.com)
--- src/components/RecipeDetail/RecipeDetail.jsx
+++ src/components/RecipeDetail/RecipeDetail.jsx
@@ -3,208 +3,229 @@ import { useSelector, useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 // MUI
 import {
-    Button, Typography,
-    Grid, Paper,
-    makeStyles, IconButton,
-    Dialog, DialogTitle, 
-    DialogActions
-} from '@material-ui/core';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import EditOutlinedIcon from '@material-ui/icons/Edit';
-import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
-import OpenInNewIcon from '@material-ui/icons/OpenInNew';
+  Button,
+  Typography,
+  Grid,
+  Paper,
+  makeStyles,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+} from "@material-ui/core";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import EditOutlinedIcon from "@material-ui/icons/Edit";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import OpenInNewIcon from "@material-ui/icons/OpenInNew";
 
-import './RecipeDetail.css';
+import "./RecipeDetail.css";
 
 const useStyles = makeStyles({
-    paper: {
-        backgroundColor: '#fff4dd',
-        margin: 'auto',
-        padding: '10px',
-        overflow: 'scroll'
-    },
-    button: {
-        marginRight: '30px',
-    },
-})
+  paper: {
+    backgroundColor: "#fff4dd",
+    margin: "auto",
+    padding: "10px",
+    overflow: "scroll",
+  },
+  button: {
+    marginRight: "30px",
+  },
+});
 
 function RecipeDetail() {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const history = useHistory();
+  const classes = useStyles();
 
-    const dispatch = useDispatch();
-    const { id } = useParams();
-    const history = useHistory();
-    const classes = useStyles();
+  const [openDeleteConfirmation, setOpenDeleteConfirmation] = useState(false);
 
-    const [openDeleteConfirmation, setOpenDeleteConfirmation] = useState(false);
+  const user = useSelector((state) => state.user);
+  const recipeDetails = useSelector((state) => state.details.recipeDetails);
+  const recipeIngredients = useSelector(
+    (state) => state.details.recipeIngredients
+  );
 
-    const user = useSelector(state => state.user);
-    const recipeDetails = useSelector(state => state.details.recipeDetails);
-    const recipeIngredients = useSelector(state => state.details.recipeIngredients);
+  useEffect(() => {
+    dispatch({ type: "FETCH_RECIPE_DETAILS", payload: id });
+    dispatch({ type: "FETCH_RECIPE_INGREDIENTS", payload: id });
+  }, [id]);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_RECIPE_DETAILS', payload: id });
-        dispatch({ type: 'FETCH_RECIPE_INGREDIENTS', payload: id });
-    }, [id]);
+  const goBack = () => {
+    history.goBack();
+  };
 
-    const goBack = () => {
-        history.goBack();
-    }
+  const handleDelete = () => {
+    setOpenDeleteConfirmation(true);
+  };
 
-    const handleDelete = () => {
-        setOpenDeleteConfirmation(true);
-    }
+  const confirmDelete = () => {
+    dispatch({ type: "DELETE_USER_RECIPE", payload: recipeDetails.id });
+    history.push("/myRecipes");
+  };
 
-    const confirmDelete = () => {
-        dispatch({ type: 'DELETE_USER_RECIPE', payload: recipeDetails.id });
-        history.push('/myRecipes');
-    }
+  const openEditRecipe = (id) => {
+    console.log("clicked edit for:", id);
+    history.push(`/editRecipe/${id}`);
+  };
 
-    const openEditRecipe = (id) => {
-        console.log('clicked edit for:', id);
-        history.push(`/editRecipe/${id}`)
-    }
+  // from stack overflow
+  // allows recipe link to open in new tab
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+  };
 
-    // from stack overflow
-    // allows recipe link to open in new tab
-    const openInNewTab = (url) => {
-        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
-        if (newWindow) newWindow.opener = null
-    }
-
-    return (
-        <div className="recipe-container">
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <IconButton
-                        color="secondary"
-                        size="medium"
-                        className={classes.button}
-                        onClick={goBack}
-                    >
-                        <ArrowBackIosIcon />
-                    </IconButton>
-                    {/* if recipe being viewed was created by logged in user, allow for delete and edit */}
-                    {user.id === recipeDetails.user_id &&
-                        <>
-                            <IconButton
-                                color="secondary"
-                                size="medium"
-                                className={classes.button}
-                                onClick={() => openEditRecipe(recipeDetails.id)}
-                            >
-                                <EditOutlinedIcon />
-                            </IconButton>
-                            <IconButton
-                                color="secondary"
-                                size="medium"
-                                className={classes.button}
-                                onClick={handleDelete}
-                            >
-                                <DeleteOutlineIcon />
-                            </IconButton>
-                        </>
-                    }
-                </Grid>
-                <Grid item xs={8}>
-                    <Typography display="inline" color="secondary" variant="h2" >{recipeDetails.name}</Typography>
-                    <Typography color="secondary" variant="subtitle1">From user:{' '}{recipeDetails.user_id}</Typography>
-                    <Typography variant="caption" color="secondary">
-                        {recipeDetails.tags}
-                    </Typography>
-                </Grid>
-                <Grid item xs={4}>
-                    <img src={recipeDetails.photo} className="recipe-img" />
-                </Grid>
-                {/* check if recipe has a grade */}
-                {recipeDetails.chef_grade === '0'
-                    ?
-                    // if recipe has no grade, show if it is set for review
-                    <Grid item xs={8} style={{ marginBottom: '40px' }}>
-                        <Typography color="secondary" display="inline">
-                            Marked for Review: {' '} {recipeDetails.marked_for_review ? 'Yes' : 'No'}
-                        </Typography>
-                    </Grid>
-                    :
-                    // if recipe has grade, show score and feedback
-                    <Grid item xs={8}>
-                        <Typography variant="h6" color="secondary" display="inline">
-                            Chef Score:
-                        </Typography>
-                        {' '}
-                        <Typography display="inline" color="secondary" variant="h5">{recipeDetails.chef_grade}</Typography>
-                        <Paper className={classes.paper} style={{ height: '100px' }}>
-                            <Typography variant="h6" color="primary">{recipeDetails.chef_feedback}</Typography>
-                        </Paper>
-                    </Grid>
-                }
-                {/* check if recipe has url */}
-                {recipeDetails.url
-                    ?
-                    // if there is a url, show button to open in new tab
-                    <Grid item xs={6}>
-                        <Button
-                            endIcon={<OpenInNewIcon />}
-                            color="secondary"
-                            // onclick open link in new tab 
-                            onClick={() => openInNewTab(recipeDetails.url)}
-                        >
-                            <Typography display="inline" color="secondary" variant="h5">Open Recipe</Typography>
-                        </Button>
-                    </Grid>
-                    :
-                    // if recipe is user created, show details
-                    <>
-                        <Grid item xs={6}>
-                            <Typography variant="h6" color="secondary">
-                                Ingredients:
-                            </Typography>
-                            <Paper className={classes.paper} style={{ height: '250px' }}>
-                                {recipeIngredients.map((ingredient, i) => {
-                                    return (
-                                        <Typography key={i} color="primary" variant="h6">
-                                            &#183;{' '}{ingredient.amount} {ingredient.unit} {ingredient.name}
-                                        </Typography>
-                                    )
-                                })}
-                            </Paper>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Typography variant="h6" color="secondary">
-                                Instructions:
-                            </Typography>
-                            <Paper className={classes.paper} style={{ height: '250px' }}>
-                                <Typography 
-                                    color="primary" 
-                                    variant="h6" 
-                                    style={{ whiteSpace: 'pre-wrap' }}
-                                >
-                                    {recipeDetails.description}
-                                </Typography>
-                            </Paper>
-                        </Grid>
-                    </>
-                }
-            </Grid>
-
-            {/* delete confirmation */}
-            <Dialog
-                maxWidth="sm"
-                open={openDeleteConfirmation}
+  return (
+    <div className="recipe-container">
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <IconButton
+            color="secondary"
+            size="medium"
+            className={classes.button}
+            onClick={goBack}
+          >
+            <ArrowBackIosIcon />
+          </IconButton>
+          {/* if recipe being viewed was created by logged in user, allow for delete and edit */}
+          {user.id === recipeDetails.user_id && (
+            <>
+              <IconButton
+                color="secondary"
+                size="medium"
+                className={classes.button}
+                onClick={() => openEditRecipe(recipeDetails.id)}
+              >
+                <EditOutlinedIcon />
+              </IconButton>
+              <IconButton
+                color="secondary"
+                size="medium"
+                className={classes.button}
+                onClick={handleDelete}
+              >
+                <DeleteOutlineIcon />
+              </IconButton>
+            </>
+          )}
+        </Grid>
+        <Grid item xs={8}>
+          <Typography display="inline" color="secondary" variant="h2">
+            {recipeDetails.name}
+          </Typography>
+          <Typography color="secondary" variant="subtitle1">
+            From user: {recipeDetails.user_id}
+          </Typography>
+          <Typography variant="caption" color="secondary">
+            {recipeDetails.tags}
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <img src={recipeDetails.photo} className="recipe-img" />
+        </Grid>
+        {/* check if recipe has a grade */}
+        {recipeDetails.chef_grade === "0" ? (
+          // if recipe has no grade, show if it is set for review
+          <Grid item xs={8} style={{ marginBottom: "40px" }}>
+            <Typography color="secondary" display="inline">
+              Marked for Review:{" "}
+              {recipeDetails.marked_for_review ? "Yes" : "No"}
+            </Typography>
+          </Grid>
+        ) : (
+          // if recipe has grade, show score and feedback
+          <Grid item xs={8}>
+            <Typography variant="h6" color="secondary" display="inline">
+              Chef Score:
+            </Typography>{" "}
+            <Typography display="inline" color="secondary" variant="h5">
+              {recipeDetails.chef_grade}
+            </Typography>
+            <Paper className={classes.paper} style={{ height: "100px" }}>
+              <Typography variant="h6" color="primary">
+                {recipeDetails.chef_feedback}
+              </Typography>
+            </Paper>
+          </Grid>
+        )}
+        {/* check if recipe has url */}
+        {recipeDetails.url ? (
+          // if there is a url, show button to open in new tab
+          <Grid item xs={6}>
+            <Button
+              endIcon={<OpenInNewIcon />}
+              color="secondary"
+              // onclick open link in new tab
+              onClick={() => openInNewTab(recipeDetails.url)}
             >
-                <div style={{ backgroundColor: '#fff4dd' }}>
-                    <DialogTitle style={{ color: '#ad4830' }}>Are you sure you want to delete this recipe?</DialogTitle>
-                    <DialogActions>
-                        <Button onClick={() => setOpenDeleteConfirmation(false)} color="primary">
-                            Cancel
-                        </Button>
-                        <Button onClick={confirmDelete} color="secondary" variant="outlined" style={{ backgroundColor: '#990f02' }}>
-                            Delete
-                        </Button>
-                    </DialogActions>
-                </div>
-            </Dialog>
-        </div >
-    )
-};
+              <Typography display="inline" color="secondary" variant="h5">
+                Open Recipe
+              </Typography>
+            </Button>
+          </Grid>
+        ) : (
+          // if recipe is user created, show details
+          <>
+            <Grid item xs={6}>
+              <Typography variant="h6" color="secondary">
+                Ingredients:
+              </Typography>
+              <Paper className={classes.paper} style={{ height: "250px" }}>
+                {recipeIngredients.map((ingredient, i) => {
+                  return (
+                    <Typography key={i} color="primary" variant="h6">
+                      &#183; {ingredient.amount} {ingredient.unit}{" "}
+                      {ingredient.name}
+                    </Typography>
+                  );
+                })}
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h6" color="secondary">
+                Instructions:
+              </Typography>
+              <Paper className={classes.paper} style={{ height: "250px" }}>
+                <Typography
+                  color="primary"
+                  variant="h6"
+                  style={{ whiteSpace: "pre-wrap" }}
+                >
+                  {recipeDetails.description}
+                </Typography>
+              </Paper>
+            </Grid>
+          </>
+        )}
+      </Grid>
+
+      {/* delete confirmation */}
+      <Dialog maxWidth="sm" open={openDeleteConfirmation}>
+        <div style={{ backgroundColor: "#fff4dd" }}>
+          <DialogTitle style={{ color: "#ad4830" }}>
+            Are you sure you want to delete this recipe?
+          </DialogTitle>
+          <DialogActions>
+            <Button
+              onClick={() => setOpenDeleteConfirmation(false)}
+              color="primary"
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={confirmDelete}
+              color="secondary"
+              variant="outlined"
+              style={{ backgroundColor: "#990f02" }}
+            >
+              Delete
+            </Button>
+          </DialogActions>
+        </div>
+      </Dialog>
+    </div>
+  );
+}
 
 export default RecipeDetail;
